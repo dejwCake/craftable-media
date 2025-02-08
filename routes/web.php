86df485470379e46fa903164d88e31b3ad42d7ed
David Běhal <david.behal@team.blue>
2025-02-08 10:01:56 +0100

    Fix readme command for tests and rewrite routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
-Route::middleware(['auth:' . config('admin-auth.defaults.guard')])->group(static function () {
-    Route::namespace('Brackets\Media\Http\Controllers')->group(static function () {
-        Route::post('upload', 'FileUploadController@upload')->name('brackets/media::upload');
-        Route::get('view', 'FileViewController@view')->name('brackets/media::view');
+use Brackets\Media\Http\Controllers\FileUploadController;
+use Brackets\Media\Http\Controllers\FileViewController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:' . config('admin-auth.defaults.guard')])
+    ->group(static function () {
+        Route::post('upload', [FileUploadController::class, 'upload'])
+            ->name('brackets/media::upload');
+        Route::get('view', [FileViewController::class, 'view'])
+            ->name('brackets/media::view');
     });
-});
